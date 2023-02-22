06111b3c24cfe88b6a5387a3cebc87b8fe899440
Nwabor Kelvin <knwabor@gmail.com>
2023-02-22 18:16:37 +0100

    Personal digital assitant

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import React from "react";
+import Alexa from "./components/Alexa";
+import Cortana from "./components/Cortana";
+import Siri from "./components/Siri";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assitant</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <Alexa
+                title="alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was designed by amazom"
+              />
+            </div>
+
+            <div className="column is-4">
+              <Cortana
+                title="cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="cortana was designed by microsoft"
+              />
+            </div>
+
+            <div className="column is-4">
+              <Siri
+                title="siri"
+                handle="@siri_1"
+                image={SiriImage}
+                description="Siri was designed by google"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
